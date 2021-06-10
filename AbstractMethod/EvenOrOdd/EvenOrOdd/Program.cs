e4f40ebeaa29f952200c2127047930c698097d2b
Laxman Pache <45862911+laxmanpache@users.noreply.github.com>
2021-06-10 19:33:13 +0530

    Add files via upload
    Even Odd Number Program

---
+++ AbstractMethod/EvenOrOdd/EvenOrOdd/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOrOdd
+{
+    class Program
+    {
+         public void evenodd(int num)
+        {
+             if(num%2==0)
+
+            {
+                Console.WriteLine("You Entered Number is even");
+            }
+             else
+            {
+                Console.WriteLine("You Entered number is odd");
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            int num;
+            Console.WriteLine("Enter number which you wnat to check even or odd");
+            num = Convert.ToInt32(Console.ReadLine());
+            Program obj = new Program();
+            obj.evenodd(num);
+            Console.ReadLine();
+        }
+    }
+}
