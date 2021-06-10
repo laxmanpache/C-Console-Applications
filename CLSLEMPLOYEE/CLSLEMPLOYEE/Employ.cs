4b62dbab623432a75cb4a18d11d15bd58867faf9
Laxman Pache <45862911+laxmanpache@users.noreply.github.com>
2021-06-10 20:08:07 +0530

    Add files via upload
    Console Based Project For Employee Operation

---
+++ CLSLEMPLOYEE/CLSLEMPLOYEE/Employ.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLSLEMPLOYEE
+{
+    public class Employ
+    {
+        public int eno { get; set; }
+        public string ename { get; set; }
+        public double Basic { get; set; }
+
+        public override string ToString()
+        {
+            return " number:" + eno + "  Name:" + ename + "  Basic:" + Basic;
+        }
+    }
+}
---
+++ CLSLEMPLOYEE/CLSLEMPLOYEE/EmployC.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLSLEMPLOYEE
+{
+    public class EmployC
+    {
+        static List<Employ> employList;
+        static EmployC()
+        {
+            employList = new List<Employ>();
+
+        }
+
+        public string Deleterec(int eno)
+        {
+            Employ obj = SerchEmploy(eno);
+            if (obj != null)
+            {
+                employList.Remove(obj);
+                return "record Deleted Successfully ";
+            }
+            else
+            {
+                return " Record not found";
+            }
+        }
+
+        public string updateemp(Employ updateEMP)
+        {
+
+            Employ res = SerchEmploy(updateEMP.eno);
+
+            if (res != null)
+
+            {
+                foreach (Employ e in employList)
+                {
+                    if (e.eno == updateEMP.eno)
+                    {
+                        //flag = true;
+                        e.ename = updateEMP.ename;
+                        e.Basic = updateEMP.Basic;
+                    }
+                }
+                return "record updated";
+
+            }
+
+            return "record not found";
+
+
+        }
+
+        public Employ SerchEmploy(int eno)
+        {
+            Employ empl = null;
+            foreach (Employ e in employList)
+            {
+                if (e.eno == eno)
+                {
+                    empl = e;
+                }
+
+
+            }
+            return empl;
+        }
+        public string AddEmploy(Employ employ)
+        {
+            employList.Add(employ);
+            return "Employee Added sucessfully";
+        }
+        public List<Employ> show()
+        {
+            return employList;
+        }
+    }
+}
