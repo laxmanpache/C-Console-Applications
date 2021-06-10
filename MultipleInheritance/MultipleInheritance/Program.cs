3f3421bf4518c61da1e7763b2e378c11ba19847e
Laxman Pache <45862911+laxmanpache@users.noreply.github.com>
2021-06-10 19:47:55 +0530

    Add files via upload
    Multiple Inheritance Example

---
+++ MultipleInheritance/MultipleInheritance/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleInheritance
+{
+    interface rat
+    {
+        void run();
+
+
+    }
+    interface tortoise
+    {
+        void name();
+    }
+
+    class animal: rat, tortoise
+    {
+        public void run()
+        {
+            Console.WriteLine("Rat run Fast");
+        }
+        public void name()
+        {
+            Console.WriteLine("Tortoise");
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            animal obj = new animal();
+            obj.run();
+            obj.name();
+            Console.ReadLine();
+        }
+    }
+}
