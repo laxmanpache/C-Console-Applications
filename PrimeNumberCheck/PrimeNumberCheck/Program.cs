dfd9917292cc09ca741ac0f5c8cd71dfe1a1532f
Laxman Pache <45862911+laxmanpache@users.noreply.github.com>
2021-06-10 19:34:29 +0530

    Add files via upload
    Prime Number Check

---
+++ PrimeNumberCheck/PrimeNumberCheck/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberCheck
+{
+    class Program
+    {
+          public void check(int num)
+        {
+            bool flag =false;
+           for(int i=2;i<=num/2;i++)
+            {
+                if(num%i==0)
+                {
+                    flag = true;
+                    break;
+                }
+            }
+           if(flag==true)
+            {
+                Console.WriteLine("This number is not prime number");
+            }
+           else
+            {
+                Console.WriteLine("this number is Prime number");
+            }
+        }
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter number which you want to check");
+            num = Convert.ToInt32(Console.ReadLine());
+            Program obj = new Program();
+            obj.check(num);
+            Console.ReadLine();
+        }
+    }
+}
