95fb6e5f5bf060cc58a9f4e74ade244424bf7901
Laxman Pache <45862911+laxmanpache@users.noreply.github.com>
2021-06-10 19:28:05 +0530

    Add files via upload

---
+++ AbstractMethod/AbstractMethod/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractMethod
+{
+
+   public abstract class Trening {
+
+        public abstract void name();
+        public abstract void email();
+    
+    }
+
+    class rahul : Trening
+    {
+        public override void email()
+        {
+            Console.WriteLine("Email is : rahul@123gmail.com");
+        }
+
+        public override void name()
+        {
+            Console.WriteLine("My name is Rahul");
+        }
+    }
+
+    public class Shyam : Trening
+    {
+        public override void email()
+        {
+            Console.WriteLine("Email is : rahul@123gmail.com");
+        }
+
+        public override void name()
+        {
+            Console.WriteLine("My name is Laxman");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trening[] arr = new Trening[] {
+
+                new rahul(),new Shyam()
+            };
+            foreach(Trening t in arr)
+            {
+                t.name();
+                t.email();
+
+            }
+       
+        Console.ReadLine();
+
+        }
+    }
+}
---
+++ ArrayList/ArrayList/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListExample
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arr = new ArrayList(10);
+            arr.Add(100);
+            Console.WriteLine(arr.Capacity);
+            arr.Add(10);
+            arr.Add(20);
+            arr.Add(40);
+            arr.Add(90);
+
+            Console.WriteLine(arr.Capacity);
+            foreach(object obj in arr)
+            {
+                Console.Write(obj + " ");
+            }
+            Console.WriteLine();
+            arr.Insert(3,200);
+            foreach (object obj in arr)
+            {
+                Console.Write(obj + " ");
+            }
+            arr.Remove(100);
+            Console.WriteLine();
+            foreach (object obj in arr)
+            {
+                Console.Write(obj + " ");
+            }
+            arr.RemoveAt(3);
+            Console.WriteLine();
+            foreach (object obj in arr)
+            {
+                Console.Write(obj + " ");
+            }
+            arr.RemoveRange(1,2);
+            Console.WriteLine();
+            foreach (object obj in arr)
+            {
+                Console.Write(obj + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
